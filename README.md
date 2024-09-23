2896243a9eff069940781603d1fe844939db1dfc
hujinbin <437044048@qq.com>
2024-09-23 20:21:27 +0800

    feat: exe打包输出和模拟点击功能

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flask
 python flask  基础框架
+
+
+### 打包输出exe文件
+> pyinstaller --onefile app.py  
--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
  
+
+from flask import Flask, request, jsonify
+import threading
+import pyautogui
+import time
+
 # 创建Flask应用实例
 app = Flask(__name__)
- 
+
+is_running = False
+
+def auto_mine():
+    global is_running
+    while is_running:
+        x, y = 100, 200 
+        pyautogui.click(x, y)
+        time.sleep(5)
+
+
 # 定义路由及其响应函数
 @app.route('/')
 def index():
@@ -11,3 +27,4 @@ def index():
 if __name__ == '__main__':
     # 运行Flask应用
     app.run(debug=True)
+    # app.run(port=5000)
