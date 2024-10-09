de383ea67767613a3868708fa7a35748f04539c1
hujinbin <437044048@qq.com>
2024-10-09 19:42:52 +0800

    feat: pywinauto和pytesseract 库

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
-import tkinter as tk
+import tkinter as tk   # tkinter 创建图形用户界面 (GUI) 的标准库
 from tkinter import messagebox
 import threading
 from flask import Flask, request, jsonify
-import pyautogui
-import cv2
+import pyautogui   # 实现自动化的图形用户界面 (GUI) 操作
+import pytesseract  # 图像 文字的识别
+import cv2   # opencv-python 是一个基于 BSD 许可（开源）发行的跨平台计算机视觉库， 图像处理和图像分析
 import numpy as np
 import time
+import random
+
+from pywinauto import Application  #pywinauto Windows应用程序自动化控制库。它支持模拟鼠标和键盘操作，实现对Windows应用程序的自动化控制
+
 
 app = Flask(__name__)
 is_running = False
+
+
+def focus_window():
+    # 启动应用程序
+    # app = Application().start("notepad.exe")
+    try:
+        # Connect to the existing application
+        curApp = Application().connect(title='记事本')
+        # Get the window object
+        app_window = curApp.window(title='记事本')
+        # Set focus to the window
+        app_window.set_focus()
+        time.sleep(random.uniform(3, 5))
+    except Exception as e:
+        print(f"Failed to focus on the window: {e}")
+
+
 
 def locate_gem(image_path):
     screenshot = pyautogui.screenshot()
@@ -22,6 +44,7 @@ def locate_gem(image_path):
 
 def auto_mine(image_path):
     global is_running
+    focus_window()
     while is_running:
         pos = locate_gem(image_path)
         if pos:
